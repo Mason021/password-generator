93a79e677113dd7bb5a4418ec6c242ad4fa74353
Mason021 <mdc5054@gmail.com>
2021-06-11 18:59:53 -0500

    working out steps and how to go about it.  starting push

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,39 @@
 // Assignment code here
 
+// GIVEN I need a new, secure password
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+window.alert("Password must be at least 8 characters and no more than 128 characters in length.  Please select from the prompts to create a new random password");
+var length = 
+var lowerCase = confirm("Include Lowercase Characters (abcd...)?")
+var upperCase = confirm("Include Uppercase Characters (ABCD...)?")
+var numeric = confirm("Include Numeric Characters (1234)?")
+var special = confirm("Include Special Characters (@#$%)")
+
+var lowerCaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericCharactersValue = Math.floor(Math.random() * 9) + 128;
+var specialCharacters = ["!","#","$","%","&","@"]
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 character
+
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
