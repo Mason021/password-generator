dceaca4944c184076ab92eb3724149836162b095
Mason021 <mdc5054@gmail.com>
2021-06-11 22:50:39 -0500

    rethought how i want to do things. going back to if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,18 @@
 // Assignment code here
 
+
 // GIVEN I need a new, secure password
 
 // WHEN I click the button to generate a password
+// Add event listener to generate button
+
 // THEN I am presented with a series of prompts for password criteria
-window.alert("Password must be at least 8 characters and no more than 128 characters in length.  Please select from the prompts to create a new random password"); 
+window.alert("Password must be at least 8 characters and no more than 128 characters in length.  Please select from the prompts to create a new random password");
+
+special = confirm("Include Special Characters (@#$%)?");
+lowerCase = confirm("Include Lowercase Characters (abcd...)?");
+upperCase = confirm("Include Uppercase Characters (ABCD...)?");
+numeric = confirm("Include Numeric Characters (1234)?");
 // prompts for user usage
 
 // var lowerCase = confirm("Include Lowercase Characters (abcd...)?")
@@ -16,20 +24,25 @@ window.alert("Password must be at least 8 characters and no more than 128 charac
 var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numericCharactersValue = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var specialCharacters = ["\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:"]
+var specialCharacters = ["\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:"];
 
-var freshPassWord = [];
+function newPassword() {
 
+//setting the length the user wants the password to be//
+  length = parseInt(prompt("Please select the number of characters ( Min 8 - Max 128 ) you would like included "));
 
-  //setting the length the user wants the password to be//
-var length = parseInt(prompt("Please select the number of characters ( Min 8 - Max 128 ) you would like included "));
-
-if (length < 8) {
-  alert("Passwords must be between 8 and 128 characters long");
-  prompt("Please select the number of characters ( Min 8 - Max 128 ) you would like included");
+  if (length < 8) {
+    alert("Passwords must be between 8 and 128 characters long");
+    prompt("Please select the number of characters ( Min 8 - Max 128 ) you would like included");
 }
-
-
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+special = confirm("Include Special Characters (@#$%)?");
+lowerCase = confirm("Include Lowercase Characters (abcd...)?");
+upperCase = confirm("Include Uppercase Characters (ABCD...)?");
+numeric = confirm("Include Numeric Characters (1234)?");
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
 
